29505b1552ca7b955f907fefcf5ca01c7f21f094
PengJian <pengjian@snda.com>
2012-12-12 16:46:37 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ku6statistics::Application.config.secret_token = 'a3d69535cdd8faca147b461632e5a5b4fb62564633bce0b0dd6255b06c8cde32085a42531a373fc185e8f21e42c9354677f289907be66efae59a5ecd6bf0920d'
