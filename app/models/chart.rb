58641abd83cb89b37caeb0a7d04434ceb04d66fb
alucardpj <alucardpj@gmail.com>
2012-12-29 15:24:26 +0800

    删除kaminari残余代码

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -2,5 +2,4 @@ class Chart < ActiveRecord::Base
   attr_accessible :name, :charttype
 
   has_many :chartnodes
-  paginates_per 20
 end
--- app/models/chartnode.rb
+++ app/models/chartnode.rb
@@ -2,5 +2,4 @@ class Chartnode < ActiveRecord::Base
   attr_accessible :chart_id, :xaxis, :yaxis
 
   belongs_to :chart
-  paginates_per 10
 end
